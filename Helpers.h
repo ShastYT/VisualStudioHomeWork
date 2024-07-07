0a4a829d7271e1b847e4f66224709bd6db1c3a18
Shast <nikita.ryndya@mail.ru>
2024-07-07 17:13:19 +0300

    HomeWork13.4

---
+++ Helpers.h
@@ -0,0 +1,4 @@
+#pragma once
+int SqrtSum(int a, int b) {
+	return (a + b)*(a + b);
+}
---
+++ HomeWork13.4.cpp
@@ -0,0 +1,14 @@
+﻿/*
+* Прощу прощения за долгое отсутствие,
+* я был сильно занят подготовкой к ЕГЭ
+* и последующей пересдачей одного из предметов
+*/
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    std::cout << "Square of sum " << a << " and " << b <<  " equal " << SqrtSum(a, b);
+}
