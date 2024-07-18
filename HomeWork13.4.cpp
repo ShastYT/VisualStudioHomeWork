9e2f5d0734df9f31e2774db694721f4930094baa
Shast <nikita.ryndya@mail.ru>
2024-07-18 13:14:40 +0300

    HomeWork17.5

--- HomeWork13.4.cpp
+++ HomeWork13.4.cpp
@@ -6,38 +6,36 @@
 #include <iostream>
 #include <time.h>
 
+class Vector3 {
+private:
+	double x;
+	double y;
+	double z;
+public:
+	Vector3() : x(0), y(0), z(0){}
+
+	Vector3(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
+	double Lenght(){
+		return pow((x * x + y * y + z * z),0.5);
+	}
+
+	void Show() 
+	{
+		std::cout << '\n' << x << " " << y << " " << z;
+	}
+
+
+};
 
 int main()
 {
-	// Получения текущего дня(?)
-	struct tm buf;
-	time_t t = time(NULL);
-	localtime_s(&buf, &t);
-	int day = buf.tm_mday;
-
-	const int N = 5; // Можно менять для изменения размера матрицы
-	int array[N][N];
-	int sum = 0;
-
-	// Вывод матрицы
-	for (int i = 0; i < N; i++) {
-
-		for (int j = 0; j < N; j++) {
-
-			array[i][j] = i + j;
-			std::cout << (i + j) << " ";
-
-		}
-
-		std::cout << "\n";
-	}
-
-	std::cout << "\n";
-	// Вывод Строки и суммы элементов (где простой sum(array)?((( )
-	for (int j = 0; j < N; j++) {
-		std::cout << array[day % N][j] << " ";
-		sum+= array[day % N][j];
-	}
-	std::cout << "\n" << sum;
+	int x = 2; //
+	int y = 3; //Длины вектора по осям xyz для 2го вывода
+	int z = 4; //
+	Vector3 V, V1(x,y,z);
+	V.Show();
+	V1.Show();
+	std::cout << '\n' << V1.Lenght();
 }
 
