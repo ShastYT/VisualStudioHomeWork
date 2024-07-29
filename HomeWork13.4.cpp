033b8833bcdaf7d70ed1a2a33ec987ba4a72074c
Shast <nikita.ryndya@mail.ru>
2024-07-29 14:32:24 +0300

    HomeWork18.5

--- HomeWork13.4.cpp
+++ HomeWork13.4.cpp
@@ -1,41 +1,91 @@
 ﻿/*
-* Прощу прощения за долгое отсутствие,
-* я был сильно занят подготовкой к ЕГЭ
-* и последующей пересдачей одного из предметов
+* Я не смог понять как приспособить Character 
+* для продвинутых типов соритровки (bubble, quick)
+* поэтому сделал простой цикл (массив вряд ли будет большой)
+* с проверкой, отсортирован ли массив
 */
 #include <iostream>
-#include <time.h>
+#include <string>
+#include <algorithm>
+using namespace std;
 
-class Vector3 {
+class Character {
 private:
-	double x;
-	double y;
-	double z;
+
+	string Name = "Defalut";
+	int Score = 0;
+
 public:
-	Vector3() : x(0), y(0), z(0){}
 
-	Vector3(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
-
-	double Lenght(){
-		return pow((x * x + y * y + z * z),0.5);
+	void SetCharacter(string NewName, int NewScore) {
+		Name = NewName;
+		Score = NewScore;
 	}
 
-	void Show() 
-	{
-		std::cout << '\n' << x << " " << y << " " << z;
+	string GetName() {
+		return Name;
 	}
 
-
+	int GetScore() {
+		return Score;
+	}
 };
+
+bool isSorted(Character** arr, int n) //Проверка, отсортирован ли массив
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (arr[i]->GetScore() > arr[i + 1]->GetScore()) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 
 int main()
 {
-	int x = 2; //
-	int y = 3; //Длины вектора по осям xyz для 2го вывода
-	int z = 4; //
-	Vector3 V, V1(x,y,z);
-	V.Show();
-	V1.Show();
-	std::cout << '\n' << V1.Lenght();
+	cout << "Amount of players: ";
+	int PlayersAmount;
+	cin >> PlayersAmount;
+
+	Character** players = new Character * [PlayersAmount];
+
+
+	// Добавление игроков
+	for (int i = 0; i < PlayersAmount; ++i) {
+		cout << "\n Player number " << i+1 << "\n";
+		players[i] = new Character();
+		string CurrentPlayerName;
+		int CurrentPlayerScore;
+
+		cout << "\n Name: ";
+		cin >> CurrentPlayerName;
+
+		cout << "\n Score: ";
+		cin >> CurrentPlayerScore;
+
+		players[i]->SetCharacter(CurrentPlayerName, CurrentPlayerScore);
+	}
+
+	//Сортировка типа a = b, b = a пока массив не отсортируется
+	while (isSorted(players,PlayersAmount) == false) {
+		for (int j = 0; j < PlayersAmount - 1; j++) {
+			if (players[j]->GetScore() > players[j + 1]->GetScore()) {
+
+				Character* temp = players[j];
+				players[j] = players[j + 1];
+				players[j + 1] = temp;
+
+			}
+		}
+	}
+	
+	// Вывод игроков в порядке возрастания счета
+	for (int i = 0; i < PlayersAmount; i++) {
+		cout << "\n" << players[i]->GetName() << " " << players[i]->GetScore() << "\n";
+	}
+
 }
 
